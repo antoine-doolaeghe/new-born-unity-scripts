63cb42cdf8f1f6797f54e1526d79c5517c3802a7
antoine-doolaeghe <antoinedoolaeghe@gmail.com>
2018-12-27 10:12:11 +0100

    fix indentatio

--- ml-agents-master/UnitySDK/Assets/NewBorn/Scripts/Api/PostGene.cs
+++ ml-agents-master/UnitySDK/Assets/NewBorn/Scripts/Api/PostGene.cs
@@ -1,69 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.Networking;
 using Gene;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
-public class PostGene : MonoBehaviour {
-    public string[] response;
-    public GameObject cell;
+namespace Gene {
+    public class PostGene : MonoBehaviour {
+        public string[] response;
+        public GameObject cell;
 
-    public IEnumerator postCell(string cellInfos, string cellName)
-	{
-		string url = "https://pnk98uo8jf.execute-api.eu-west-2.amazonaws.com/prod/cell";
-        PostObject postObject = new PostObject(cellInfos, cellName);
-
-        string jsonStringTrial = JsonUtility.ToJson(postObject);
-    
-        UnityWebRequest www = UnityWebRequest.Put(url, jsonStringTrial);
-      
-
-        yield return www.SendWebRequest();
-
-		if (www.isNetworkError || www.isHttpError)
-		{
-            Debug.Log("error");
-		}
-	}
-
-    public IEnumerator getCell(string id)
-    {
-        Debug.Log("Helloooo");
-        using (UnityWebRequest www = UnityWebRequest.Get("https://pnk98uo8jf.execute-api.eu-west-2.amazonaws.com/prod/cell/" + id))
-        {
-            yield return www.Send();
-
-            if(www.isDone) {
-                response = www.downloadHandler.text.Split('A');
+        public IEnumerator postCell (string cellInfos, string cellName) {
+            string url = "https://pnk98uo8jf.execute-api.eu-west-2.amazonaws.com/prod/cell";
+            PostObject postObject = new PostObject (cellInfos, cellName);
+            string jsonStringTrial = JsonUtility.ToJson (postObject);
+            UnityWebRequest www = UnityWebRequest.Put (url, jsonStringTrial);
+            yield return www.SendWebRequest ();
+            if (www.isNetworkError || www.isHttpError) {
+                Debug.Log ("error");
             }
+        }
 
-            if (www.isNetworkError || www.isHttpError)
-            {
-                Debug.Log(www.error);
-            }
-            else
-            {
-                // Show results as text
-                Debug.Log(www.downloadHandler.text);
+        public IEnumerator getCell (string id) {
+            // TEMPORARY URL
+            using (UnityWebRequest www = UnityWebRequest.Get ("https://pnk98uo8jf.execute-api.eu-west-2.amazonaws.com/prod/cell/" + id)) {
+                yield return www.Send ();
 
-                // Or retrieve results as binary data
-                byte[] results = www.downloadHandler.data;
+                if (www.isNetworkError || www.isHttpError) {
+                    Debug.Log (www.error);
+                } else {
+                    if (www.isDone) {
+                        response = www.downloadHandler.text.Split ('A'); // A IS THE SPLITTER KEY BETWEEN INFOS
+                    }
+                }
             }
         }
     }
 
-    void AddCellInfo() {
-        Debug.Log(cell);
-    }
-}
+    public class PostObject {
+        public string cellInfos;
+        public string cellName;
 
-public class PostObject {
-    public string cellInfos;
-    public string cellName;
-
-    public PostObject(string cellInfos, string cellName) {
-        this.cellInfos = cellInfos;
-        this.cellName = cellName; 
+        public PostObject (string cellInfos, string cellName) {
+            this.cellInfos = cellInfos;
+            this.cellName = cellName;
+        }
     }
 }
