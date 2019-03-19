24bfba43b2229c5e0042a413556734e9f0db7dcf
antoine.doolaeghe <antoine.doolaeghe@draysonhealth.com>
2019-03-19 23:11:51 +0000

    added camera controller

---
+++ CameraController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(0,6f*Time.deltaTime,0);
+    }
+}
