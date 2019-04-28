f037a44897e2264027457dcfa37cfcf1b9423958
Antoine Doolaeghe <antoine.doolaeghe@draysonhealth.com>
2019-04-28 18:43:35 +0100

    NEW-54: fixed position api

--- Api/GenerationService.cs
+++ Api/GenerationService.cs
@@ -13,46 +13,58 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-namespace Gene {
-    [ExecuteInEditMode]
-    public class GenerationService : MonoBehaviour {
-        public ApiConfig apiConfig;
-        private String graphQlInput;
-        public List<string> generations;
-        public static Dictionary<string, string> variable = new Dictionary<string, string> ();
-        public static Dictionary<string, string[]> array = new Dictionary<string, string[]> ();
+namespace Gene
+{
+  [ExecuteInEditMode]
+  public class GenerationService : MonoBehaviour
+  {
+    public ApiConfig apiConfig;
+    private String graphQlInput;
+    public List<string> generations;
+    public static Dictionary<string, string> variable = new Dictionary<string, string>();
+    public static Dictionary<string, string[]> array = new Dictionary<string, string[]>();
 
-        public IEnumerator GetGenerations () {
-            byte[] postData;
-            Dictionary<string, string> postHeader;
-            WWW www;
-            generations = new List<string> ();
-            ServiceHelpers.graphQlApiRequest (variable, array, out postData, out postHeader, out www, out graphQlInput, apiConfig.generationsGraphQlQuery, apiConfig.apiKey, apiConfig.url);
-            yield return www;
-            if (www.error != null) {
-                throw new Exception ("There was an error sending request: " + www.error);
-            } else {
-                foreach (var generation in JSON.Parse (www.text) ["data"]["listGenerations"]["items"].AsArray) {
-                    generations.Add (generation.Value["id"]);
-                }
-            }
+    public IEnumerator GetGenerations()
+    {
+      byte[] postData;
+      Dictionary<string, string> postHeader;
+      WWW www;
+      generations = new List<string>();
+      ServiceHelpers.graphQlApiRequest(variable, array, out postData, out postHeader, out www, out graphQlInput, apiConfig.generationsGraphQlQuery, apiConfig.apiKey, apiConfig.url);
+      Debug.Log(apiConfig.apiKey);
+      yield return www;
+      if (www.error != null)
+      {
+        throw new Exception("There was an error sending request: " + www.error);
+      }
+      else
+      {
+        foreach (var generation in JSON.Parse(www.text)["data"]["listGenerations"]["items"].AsArray)
+        {
+          generations.Add(generation.Value["id"]);
         }
-
-        public IEnumerator PostGeneration (string generationId) {
-            byte[] postData;
-            Dictionary<string, string> postHeader;
-
-            GenerationService.variable["id"] = generationId;
-
-            WWW www;
-            ServiceHelpers.graphQlApiRequest (GenerationService.variable, GenerationService.array, out postData, out postHeader, out www, out graphQlInput, apiConfig.generationsGraphQlMutation, apiConfig.apiKey, apiConfig.url);
-            yield return www;
-            if (www.error != null) {
-                throw new Exception ("There was an error sending request: " + www.error);
-            } else {
-                Debug.Log (JSON.Parse (www.text) ["data"]["createGeneration"]["id"]);
-                generations.Add (JSON.Parse (www.text) ["data"]["createGeneration"]["id"]);
-            }
-        }
+      }
     }
+
+    public IEnumerator PostGeneration(string generationId)
+    {
+      byte[] postData;
+      Dictionary<string, string> postHeader;
+
+      GenerationService.variable["id"] = generationId;
+
+      WWW www;
+      ServiceHelpers.graphQlApiRequest(GenerationService.variable, GenerationService.array, out postData, out postHeader, out www, out graphQlInput, apiConfig.generationsGraphQlMutation, apiConfig.apiKey, apiConfig.url);
+      yield return www;
+      if (www.error != null)
+      {
+        throw new Exception("There was an error sending request: " + www.error);
+      }
+      else
+      {
+        Debug.Log(JSON.Parse(www.text)["data"]["createGeneration"]["id"]);
+        generations.Add(JSON.Parse(www.text)["data"]["createGeneration"]["id"]);
+      }
+    }
+  }
 }
--- Api/NewbornService.cs
+++ Api/NewbornService.cs
@@ -13,32 +13,36 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-namespace Gene {
+namespace Gene
+{
   [ExecuteInEditMode]
-  public class NewbornService : MonoBehaviour {
+  public class NewbornService : MonoBehaviour
+  {
     public string responseUuid;
     public List<float> cellInfoResponse;
     public GameObject cell;
     public ApiConfig apiConfig;
-    public delegate void QueryComplete ();
+    public delegate void QueryComplete();
     public static event QueryComplete onQueryComplete;
 
     public enum Status { Neutral, Loading, Complete, Error };
 
- public static Dictionary<string, string> variable = new Dictionary<string, string> ();
- public static Dictionary<string, string[]> array = new Dictionary<string, string[]> ();
+    public static Dictionary<string, string> variable = new Dictionary<string, string>();
+    public static Dictionary<string, string[]> array = new Dictionary<string, string[]>();
 
- private TrainingManager trainingManager;
- private NewBornBuilder newBornBuilder;
+    private TrainingManager trainingManager;
+    private NewBornBuilder newBornBuilder;
 
- private String graphQlInput;
+    private String graphQlInput;
 
- void Awake () {
- trainingManager = GameObject.Find ("TrainingManager").transform.GetComponent<TrainingManager> ();
- newBornBuilder = transform.GetComponent<NewBornBuilder> ();
+    void Awake()
+    {
+      trainingManager = GameObject.Find("TrainingManager").transform.GetComponent<TrainingManager>();
+      newBornBuilder = transform.GetComponent<NewBornBuilder>();
     }
 
-    public IEnumerator PostNewborn (NewBornPostData newBornPostData, int agentId) {
+    public IEnumerator PostNewborn(NewBornPostData newBornPostData, int agentId)
+    {
       byte[] postData;
       Dictionary<string, string> postHeader;
 
@@ -47,81 +51,104 @@ namespace Gene {
       NewbornService.variable["newbornGenerationId"] = newBornPostData.generationId;
 
       WWW www;
-      ServiceHelpers.graphQlApiRequest (variable, array, out postData, out postHeader, out www, out graphQlInput, apiConfig.newBornGraphQlMutation, apiConfig.apiKey, apiConfig.url);
+      ServiceHelpers.graphQlApiRequest(variable, array, out postData, out postHeader, out www, out graphQlInput, apiConfig.newBornGraphQlMutation, apiConfig.apiKey, apiConfig.url);
+      Debug.Log(graphQlInput);
       yield return www;
-      if (www.error != null) {
-        throw new Exception ("There was an error sending request: " + www.error);
-      } else {
-        string createdNewBornId = JSON.Parse (www.text) ["data"]["createNewborn"]["id"];
-        newBornBuilder.PostNewbornModel (createdNewBornId, 0, agentId); // will it always be first generation
+      if (www.error != null)
+      {
+        Debug.Log(www.text);
+        throw new Exception("There was an error sending request: " + www.error);
+      }
+      else
+      {
+        string createdNewBornId = JSON.Parse(www.text)["data"]["createNewborn"]["id"];
+        newBornBuilder.PostNewbornModel(createdNewBornId, 0, agentId); // will it always be first generation
       }
     }
 
 
-    public IEnumerator PostNewbornModel (GenerationPostData generationPostData, string modelId, int agentId) {
+    public IEnumerator PostNewbornModel(GenerationPostData generationPostData, string modelId, int agentId)
+    {
       byte[] postData;
 
       Dictionary<string, string> postHeader;
-
+      string cellPositionsString = "[";
+      for (int i = 0; i < generationPostData.cellPositions.Count; i++)
+      {
+        cellPositionsString = cellPositionsString + JSON.Parse(JsonUtility.ToJson(generationPostData.cellPositions[i]))["position"].ToString() + ",";
+      }
+      cellPositionsString = cellPositionsString + "]";
+      Debug.Log(cellPositionsString);
       NewbornService.variable["id"] = generationPostData.id;
       NewbornService.variable["modelNewbornId"] = modelId;
-      NewbornService.variable["cellPositions"] = JSON.Parse (JsonUtility.ToJson (generationPostData)) ["cellPositions"].ToString ();
-      NewbornService.variable["cellInfos"] = JSON.Parse (JsonUtility.ToJson (generationPostData)) ["cellInfos"].ToString ();
+      NewbornService.variable["cellPositions"] = cellPositionsString;
+      NewbornService.variable["cellInfos"] = JSON.Parse(JsonUtility.ToJson(generationPostData))["cellInfos"].ToString();
 
       WWW www;
-      ServiceHelpers.graphQlApiRequest (NewbornService.variable, NewbornService.array, out postData, out postHeader, out www, out graphQlInput, apiConfig.modelGraphQlMutation, apiConfig.apiKey, apiConfig.url);
+      ServiceHelpers.graphQlApiRequest(NewbornService.variable, NewbornService.array, out postData, out postHeader, out www, out graphQlInput, apiConfig.modelGraphQlMutation, apiConfig.apiKey, apiConfig.url);
 
       yield return www;
-      if (www.error != null) {
-        throw new Exception ("There was an error sending request: " + www.error);
-      } else {
-        Debug.Log ("New Generation successfully posted!");
-        Transform[] childs = transform.Cast<Transform> ().ToArray ();
-        DestroyAgent (childs);
-        cellInfoResponse = new List<float> ();
-        JSONNode responseData = JSON.Parse (www.text) ["data"]["createModel"];
+      if (www.error != null)
+      {
+        throw new Exception("There was an error sending request: " + www.error);
+      }
+      else
+      {
+        Debug.Log("New Generation successfully posted!");
+        Transform[] childs = transform.Cast<Transform>().ToArray();
+        DestroyAgent(childs);
+        cellInfoResponse = new List<float>();
+        JSONNode responseData = JSON.Parse(www.text)["data"]["createModel"];
         string responseId = responseData["id"];
-        foreach (var cellInfo in responseData["cellInfos"].AsArray) {
-          cellInfoResponse.Add (cellInfo.Value.AsFloat);
+        foreach (var cellInfo in responseData["cellInfos"].AsArray)
+        {
+          cellInfoResponse.Add(cellInfo.Value.AsFloat);
         }
 
         trainingManager.requestApiData = true;
-        trainingManager.BuildNewBornFromFetch (true, responseId, agentId);
+        trainingManager.BuildNewBornFromFetch(true, responseId, agentId);
       }
     }
 
-    public IEnumerator GetNewborn (string id, int agentId, bool IsGetAfterPost) {
+    public IEnumerator GetNewborn(string id, int agentId, bool IsGetAfterPost)
+    {
       byte[] postData;
       Dictionary<string, string> postHeader;
 
       NewbornService.variable["id"] = id;
 
       WWW www;
-      ServiceHelpers.graphQlApiRequest (NewbornService.variable, NewbornService.array, out postData, out postHeader, out www, out graphQlInput, apiConfig.newBornGraphQlQuery, apiConfig.apiKey, apiConfig.url);
+      ServiceHelpers.graphQlApiRequest(NewbornService.variable, NewbornService.array, out postData, out postHeader, out www, out graphQlInput, apiConfig.newBornGraphQlQuery, apiConfig.apiKey, apiConfig.url);
 
       yield return www;
-      if (www.error != null) {
-        throw new Exception ("There was an error sending request: " + www.error);
-      } else {
-        Debug.Log ("NewBorn successfully requested!");
-        cellInfoResponse = new List<float> ();
-        JSONNode responseData = JSON.Parse (www.text) ["data"]["getNewborn"];
+      if (www.error != null)
+      {
+        throw new Exception("There was an error sending request: " + www.error);
+      }
+      else
+      {
+        Debug.Log("NewBorn successfully requested!");
+        cellInfoResponse = new List<float>();
+        JSONNode responseData = JSON.Parse(www.text)["data"]["getNewborn"];
         string responseId = responseData["id"];
-        foreach (var cellInfo in responseData["models"]["items"][0]["cellInfos"].AsArray) {
-          cellInfoResponse.Add (cellInfo.Value.AsFloat);
+        foreach (var cellInfo in responseData["models"]["items"][0]["cellInfos"].AsArray)
+        {
+          cellInfoResponse.Add(cellInfo.Value.AsFloat);
         }
 
         trainingManager.requestApiData = true;
-        trainingManager.BuildNewBornFromFetch (false, responseId, agentId);
+        trainingManager.BuildNewBornFromFetch(false, responseId, agentId);
       }
     }
 
-    public void DestroyAgent (Transform[] childs) {
-      transform.gameObject.SetActive (true);
-      transform.GetComponent<NewBornBuilder> ().DeleteCells ();
-      transform.GetComponent<AgentTrainBehaviour> ().DeleteBodyParts ();
-      foreach (Transform child in childs) {
-        DestroyImmediate (child.gameObject);
+    public void DestroyAgent(Transform[] childs)
+    {
+      transform.gameObject.SetActive(true);
+      transform.GetComponent<NewBornBuilder>().DeleteCells();
+      transform.GetComponent<AgentTrainBehaviour>().DeleteBodyParts();
+      foreach (Transform child in childs)
+      {
+        DestroyImmediate(child.gameObject);
       }
     }
   }
